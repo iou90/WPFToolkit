d7b20f3bf79bc19463a056092c47910f3f31a92d
iou90 kant <iou90@outlook.com>
2016-07-19 14:27:39 +0800

    add a new meature string method

--- Kant.Wpf.Toolkit/Kant.Wpf.Toolkit/MeasureHepler.cs
+++ Kant.Wpf.Toolkit/Kant.Wpf.Toolkit/MeasureHepler.cs
@@ -16,5 +16,64 @@ namespace Kant.Wpf.Toolkit
 
             return new Size(formattedText.Width, formattedText.Height);
         }
+
+        public static Size MeasureString(string text, Style style, CultureInfo culture)
+        {
+            if (style == null || style.Setters == null)
+            {
+                throw new ArgumentNullException("style or style.Setters is null");
+            }
+
+            var flowDirection = FlowDirection.LeftToRight;
+            var fontFamily = new FontFamily("Segoe UI");
+            var fontSize = 12.0;
+            var fontStyle = FontStyles.Normal;
+            var fontWeight = FontWeights.Regular;
+            var fontStretch = FontStretches.Normal;
+            Brush foregroud = new SolidColorBrush(Colors.Black);
+
+            foreach (var s in style.Setters)
+            {
+                var setter = (Setter)s;
+
+                switch (setter.Property.Name)
+                {
+                    case "FlowDirectoin":
+                        flowDirection = (FlowDirection)setter.Value;
+
+                        break;
+                    case "FontFamily":
+                        fontFamily = (FontFamily)setter.Value;
+
+                        break;
+                    case "FontSize":
+                        fontSize = (double)setter.Value;
+
+                        break;
+                    case "FontStyle":
+                        fontStyle = (FontStyle)setter.Value;
+
+                        break;
+                    case "FontWeight":
+                        fontWeight = (FontWeight)setter.Value;
+
+                        break;
+                    case "FontStretch":
+                        fontStretch = (FontStretch)setter.Value;
+
+                        break;
+                    case "Foreground":
+                        foregroud = (Brush)setter.Value;
+
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            var formattedText = new FormattedText(text, culture, flowDirection, new Typeface(fontFamily, fontStyle, fontWeight, fontStretch), fontSize, foregroud);
+
+            return new Size(formattedText.Width, formattedText.Height);
+        }
     }
 }
